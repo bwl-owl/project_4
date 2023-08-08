9967c99c293347e004573626542e1631296ce821
James Preston <preston_104@hotmail.com>
2023-08-08 20:56:45 +1000

    adding in intial commit for my project 4

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,94 @@ function OtherPage() {
 }
 
 function SearchPage() {
-  return <div>This is the search page.</div>;
+  const [country, setCountry] = React.useState('');
+  const [thematic, setThematic] = React.useState('');
+  const [minPrice, setMinPrice] = React.useState('');
+  const [maxPrice, setMaxPrice] = React.useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Country: ${country}\nThematic: ${thematic}\nMin Price: ${minPrice}\nMax Price: ${maxPrice}`);
+  };
+
+  return (
+    <div>
+      <h2>Input Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Country:</label>
+          <select value={country} onChange={(e) => setCountry(e.target.value)}>
+            <option value="" disabled>Select a country</option>
+            <option value="Argentina">Argentina</option>
+            <option value="Australia">Australia</option>
+            <option value="Brazil">Brazil</option>
+            <option value="Canada">Canada</option>
+            <option value="China">China</option>
+            <option value="France">France</option>
+            <option value="Germany">Germany</option>
+            <option value="India">India</option>
+            <option value="Indonesia">Indonesia</option>
+            <option value="Italy">Italy</option>
+            <option value="Japan">Japan</option>
+            <option value="Mexico">Mexico</option>
+            <option value="Russia">Russia</option>
+            <option value="Saudi Arabia">Saudi Arabia</option>
+            <option value="South Africa">South Africa</option>
+            <option value="South Korea">South Korea</option>
+            <option value="Turkey">Turkey</option>
+            <option value="United Kingdom">United Kingdom</option>
+            <option value="United States">United States</option>
+            <option value="European Union">European Union</option>
+          </select>
+        </div>
+        <div>
+          <label>Thematic:</label>
+          <select value={thematic} onChange={(e) => setThematic(e.target.value)}>
+            <option value="" disabled>Select a sector</option>
+            <option value="Energy">Energy</option>
+            <option value="Materials">Materials</option>
+            <option value="Industrials">Industrials</option>
+            <option value="Consumer Discretionary">Consumer Discretionary</option>
+            <option value="Consumer Staples">Consumer Staples</option>
+            <option value="Health Care">Health Care</option>
+            <option value="Financials">Financials</option>
+            <option value="Information Technology">Information Technology</option>
+            <option value="Communication Services">Communication Services</option>
+            <option value="Utilities">Utilities</option>
+            <option value="Real Estate">Real Estate</option>
+          </select>
+        </div>
+        <div>
+          <label>Price:</label>
+          <div>
+            <label>Min:</label>
+            <input
+              type="number"
+              value={minPrice}
+              onChange={(e) => setMinPrice(e.target.value)}
+              placeholder="Min Price"
+            />
+          </div>
+          <div>
+            <label>Max:</label>
+            <input
+              type="number"
+              value={maxPrice}
+              onChange={(e) => setMaxPrice(e.target.value)}
+              placeholder="Max Price"
+            />
+          </div>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
+
+
+
+
+
 function LoginPage() {
   return <div>This is the login page.</div>;
 }
