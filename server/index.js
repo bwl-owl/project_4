de498f09ea1fd577860ff3079fb33fd7cf25f032
bwl-owl <beiwei.lin@generalassemb.ly>
2023-08-19 09:37:16 +1000

    cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import { supabase } from "./supabase.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static("../client/dist"));
+
+app.use(
+  cors({
+    origin: "https://p4-static.onrender.com",
+  })
+);
+app.options("*", cors());
 
 app.get("/test", async (req, res) => {
   const { data } = await supabase.from("test").select();
